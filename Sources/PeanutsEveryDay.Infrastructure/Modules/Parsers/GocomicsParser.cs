5cd3e6828545de3f137f2ebb51bea2dd675b21df
Reswero <reswero@mail.ru>
2023-07-02 21:50:15 +0600

    Added Gocomics parser

---
+++ Sources/PeanutsEveryDay.Infrastructure/Modules/Parsers/GocomicsParser.cs
@@ -0,0 +1,134 @@
+﻿using HtmlAgilityPack;
+using PeanutsEveryDay.Abstraction;
+using PeanutsEveryDay.Application.Exceptions;
+using PeanutsEveryDay.Application.Modules.Parsers.Models;
+using PeanutsEveryDay.Application.Modules.Parsers;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace PeanutsEveryDay.Infrastructure.Modules.Parsers;
+
+public class GocomicsParser : IComicsParser
+{
+    private const int _maxAttemptsCount = 5;
+    private const int _maxSkippedComics = 7;
+
+    private const string _baseUrl = "https://www.gocomics.com/";
+    private const string _peanutsUrl = $"{_baseUrl}peanuts/";
+    private const string _peanutsBeginsUrl = $"{_baseUrl}peanuts-begins/";
+    private const string _baseFolder = "comics/gocomics/";
+    private const string _peanutsFolder = $"{_baseFolder}peanuts";
+    private const string _peanutsBeginsFolder = $"{_baseFolder}peanutsbegins";
+
+    private const string _xpathToPeanutsImg
+        = "//a[@title='Peanuts']/picture[@class='item-comic-image']/img";
+    private const string _xpathToPeanutsBeginsImg
+        = "//a[@title='Peanuts Begins']/picture[@class='item-comic-image']/img";
+
+    private readonly TimeSpan _defaultRequestDelay = TimeSpan.FromMilliseconds(300);
+
+    public GocomicsParser()
+    {
+        Directory.CreateDirectory(_peanutsFolder);
+        Directory.CreateDirectory(_peanutsBeginsFolder);
+    }
+
+    public async IAsyncEnumerable<ParsedComic> ParseAsync([EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        await foreach (var comic in ParseComicsAsync(_peanutsUrl, _peanutsFolder,
+            _xpathToPeanutsImg, begins: false, cancellationToken))
+        {
+            yield return comic;
+        }
+    }
+
+    public async IAsyncEnumerable<ParsedComic> ParseBeginsAsync([EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        await foreach (var comic in ParseComicsAsync(_peanutsBeginsUrl, _peanutsBeginsFolder,
+            _xpathToPeanutsBeginsImg, begins: true, cancellationToken))
+        {
+            yield return comic;
+        }
+    }
+
+    private async IAsyncEnumerable<ParsedComic> ParseComicsAsync(string baseUrl, string folder,
+        string xpathToImg, bool begins, [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        DateOnly lastComicDate = DateOnly.FromDateTime(DateTime.Now);
+
+        using HttpClientHandler clientHandler = new() { AllowAutoRedirect = false };
+        using HttpClient client = new(clientHandler);
+
+        int attemptsCount = default;
+        int skippedComics = default;
+
+        DateOnly lastParsedComic = new(1950, 10, 1);
+        DateOnly currentComic = lastParsedComic;
+
+        TimeSpan requestDelay = _defaultRequestDelay;
+
+        while (lastParsedComic < lastComicDate && currentComic <= lastComicDate)
+        {
+            currentComic = currentComic.AddDays(1);
+
+            var comicUrl
+                = $"{baseUrl}{currentComic.Year}/{currentComic.Month}/{currentComic.Day}";
+
+            using HttpResponseMessage status = await client.GetAsync(comicUrl, cancellationToken);
+
+            if (status.StatusCode == HttpStatusCode.OK)
+            {
+                HtmlWeb web = new();
+                HtmlDocument doc = await web.LoadFromWebAsync(comicUrl, cancellationToken);
+
+                string comicImageSrc = doc.DocumentNode
+                    .SelectSingleNode(xpathToImg)
+                    .Attributes["src"]
+                    .Value;
+
+                string imagePath = $"{folder}/{currentComic:yyyy_MM_dd}.png";
+
+                using HttpResponseMessage response = await client.GetAsync(comicImageSrc, cancellationToken);
+                using Stream stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+                // Sometimes the image may not load correctly
+                if (stream.Length > 1024)
+                {
+                    var sourceType = begins is true ? SourceType.GocomicsBegins : SourceType.Gocomics;
+
+                    yield return new ParsedComic(currentComic, sourceType, comicUrl, imagePath, stream);
+
+                    attemptsCount = default;
+                    skippedComics = default;
+                    lastParsedComic = currentComic;
+                    requestDelay = _defaultRequestDelay;
+                }
+            }
+            else if (status.StatusCode == HttpStatusCode.Redirect)
+            {
+                attemptsCount = default;
+                skippedComics++;
+
+                if (skippedComics > _maxSkippedComics)
+                {
+                    yield break;
+                }
+            }
+            else
+            {
+                if (attemptsCount < _maxAttemptsCount)
+                {
+                    attemptsCount++;
+                    requestDelay = _defaultRequestDelay * Math.Pow(attemptsCount, 2);
+                }
+                else
+                {
+                    throw new AttemptsExceededException();
+                }
+            }
+
+            // Avoiding ban
+            await Task.Delay(requestDelay, cancellationToken);
+        }
+    }
+}
---
+++ Tests/PeanutsEveryDay.Infrastructure.Tests/GocomicsParserTests.cs
@@ -0,0 +1,43 @@
+﻿using PeanutsEveryDay.Abstraction;
+using PeanutsEveryDay.Infrastructure.Modules.Parsers;
+
+namespace PeanutsEveryDay.Infrastructure.Tests;
+
+public class GocomicsParserTests
+{
+    [Fact]
+    public async Task Peanuts_Comics_Parsed()
+    {
+        // Arrange
+        GocomicsParser parser = new();
+
+        // Act
+        var enumerator = parser.ParseAsync().GetAsyncEnumerator();
+        await enumerator.MoveNextAsync();
+
+        var parsedComic = enumerator.Current;
+
+        // Assert
+        Assert.True(parsedComic.Source == SourceType.Gocomics);
+        Assert.NotNull(parsedComic.Url);
+        Assert.NotNull(parsedComic.ImageStream);
+    }
+
+    [Fact]
+    public async Task PeanutsBegins_Comics_Parsed()
+    {
+        // Arrange
+        GocomicsParser parser = new();
+
+        // Act
+        var enumerator = parser.ParseBeginsAsync().GetAsyncEnumerator();
+        await enumerator.MoveNextAsync();
+
+        var parsedComic = enumerator.Current;
+
+        // Assert
+        Assert.True(parsedComic.Source == SourceType.GocomicsBegins);
+        Assert.NotNull(parsedComic.Url);
+        Assert.NotNull(parsedComic.ImageStream);
+    }
+}
