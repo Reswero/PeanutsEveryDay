efd684d2c77656bbdd12f1ec60fd183e280ca299
Reswero <reswero@mail.ru>
2023-07-08 13:15:05 +0600

    Added Comic image converter

--- Sources/PeanutsEveryDay.Infrastructure/Modules/Services/ComicsLoaderService.cs
+++ Sources/PeanutsEveryDay.Infrastructure/Modules/Services/ComicsLoaderService.cs
@@ -1,4 +1,5 @@
-﻿using PeanutsEveryDay.Application.Modules.Parsers;
+﻿using PeanutsEveryDay.Application.Modules.Converters;
+using PeanutsEveryDay.Application.Modules.Parsers;
 using PeanutsEveryDay.Application.Modules.Parsers.Models;
 using PeanutsEveryDay.Application.Modules.Services;
 
@@ -7,10 +8,12 @@ namespace PeanutsEveryDay.Infrastructure.Modules.Services;
 public class ComicsLoaderService : IComicsLoaderService
 {
     private readonly IComicsParser[] _parsers;
+    private readonly IComicImageConverter _converter;
 
-    public ComicsLoaderService(IComicsParser[] parsers)
+    public ComicsLoaderService(IComicsParser[] parsers, IComicImageConverter converter)
     {
         _parsers = parsers;
+        _converter = converter;
     }
 
     public async Task LoadAsync(CancellationToken cancellationToken = default)
@@ -32,7 +35,7 @@ public class ComicsLoaderService : IComicsLoaderService
     {
         await foreach (var comic in comics)
         {
-
+            await _converter.ConvertFromStripToSquareAsync(comic.ImageStream);
         }
     }
 }
---
+++ Tests/PeanutsEveryDay.Infrastructure.Tests/ComicConverterTests.cs
@@ -0,0 +1,50 @@
+﻿using PeanutsEveryDay.Infrastructure.Modules.Converters;
+
+namespace PeanutsEveryDay.Infrastructure.Tests;
+
+public class ComicConverterTests
+{
+    [Fact]
+    public async Task Image_ShouldBe_Resized()
+    {
+        // Arrange
+        ComicConverter converter = new();
+
+        int border = 5;
+
+        Stream stream = new MemoryStream();
+        Image<Rgba64> img = new(1200, 300);
+        img.SaveAsPng(stream);
+        stream.Seek(0, SeekOrigin.Begin);
+
+        // Act
+        await converter.ConvertFromStripToSquareAsync(stream);
+
+        Image converterdImg = Image.Load(stream);
+
+        // Assert
+        Assert.True(converterdImg.Width == 500 + border * 2);
+        Assert.True(converterdImg.Height == 400 + border * 4);
+    }
+
+    [Fact]
+    public async Task Image_ShouldNotBe_Resized()
+    {
+        // Arrange
+        ComicConverter converter = new();
+
+        Stream stream = new MemoryStream();
+        Image<Rgba64> img = new(1200, 500);
+        img.SaveAsPng(stream);
+        stream.Seek(0, SeekOrigin.Begin);
+
+        // Act
+        await converter.ConvertFromStripToSquareAsync(stream);
+
+        Image converterdImg = Image.Load(stream);
+
+        // Assert
+        Assert.True(converterdImg.Width == img.Width);
+        Assert.True(converterdImg.Height == img.Height);
+    }
+}
