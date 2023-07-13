72dace095a4d8a8ea37e5b797b4f678b5e3e4246
Reswero <reswero@mail.ru>
2023-07-13 22:30:47 +0600

    First init of TelegramBot

---
+++ Sources/PeanutsEveryDay.Infrastructure/Modules/Telegram/TelegramBot.cs
@@ -0,0 +1,39 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace PeanutsEveryDay.Infrastructure.Modules.Telegram;
+
+public class TelegramBot : IUpdateHandler
+{
+    private readonly IServiceProvider _services;
+
+    private readonly TelegramBotClient _bot;
+    private readonly ReceiverOptions _receiverOptions = new()
+    {
+        AllowedUpdates = new[] { UpdateType.Message, UpdateType.CallbackQuery }
+    };
+
+    public TelegramBot(string apiKey, IServiceProvider services)
+    {
+        _services = services;
+
+        _bot = new(apiKey);
+        _bot.StartReceiving(HandleUpdateAsync, HandlePollingErrorAsync, _receiverOptions);
+    }
+
+    public async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, CancellationToken cancellationToken)
+    {
+        if (update.Message is not { } message)
+            return;
+
+        long chatId = message.Chat.Id;
+        await bot.SendTextMessageAsync(chatId, message.Text!);
+    }
+
+    public Task HandlePollingErrorAsync(ITelegramBotClient bot, Exception exception, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
