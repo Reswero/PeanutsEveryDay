32484057ae562fca397b9c21e2c20c84ed8f2dba
Reswero <reswero@mail.ru>
2023-07-14 21:25:51 +0600

    Changing User model

--- Sources/PeanutsEveryDay.Domain/Models/User.cs
+++ Sources/PeanutsEveryDay.Domain/Models/User.cs
@@ -5,4 +5,23 @@ public class User
     public required long Id { get; init; }
     public required string FirstName { get; init; }
     public string? Username { get; init; }
+
+    public required UserProgress Progress { get; init; }
+    public required UserSettings Settings { get; init; }
+
+    public static User Create(long Id, string firstName, string? username)
+    {
+        UserProgress progress = new() { UserId = Id };
+        UserSettings settings = new() { UserId = Id };
+        User user = new()
+        {
+            Id = Id,
+            FirstName = firstName,
+            Username = username,
+            Progress = progress,
+            Settings = settings
+        };
+
+        return user;
+    }
 }
