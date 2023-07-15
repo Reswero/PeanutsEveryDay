eb6d32c4cb2df14f625ba760ac6ab5b7b521def3
Reswero <reswero@mail.ru>
2023-07-15 23:52:26 +0600

    Added sources editing

---
+++ Sources/PeanutsEveryDay.Infrastructure/Modules/Telegram/CallbackDictionary.cs
@@ -0,0 +1,14 @@
+﻿namespace PeanutsEveryDay.Infrastructure.Modules.Telegram;
+
+public static class CallbackDictionary
+{
+    public const string Sources = "srcs";
+    public const string SourcePrefix = "src_";
+    public const string AcomicsSource = $"{SourcePrefix}acm";
+    public const string AcomicsBeginsSource = $"{SourcePrefix}acmB";
+    public const string GocomicsSource = $"{SourcePrefix}gcm";
+    public const string GocomicsBeginsSource = $"{SourcePrefix}gcmB";
+    public const string BackFromSources = "bckFrSrcs";
+
+    public const string Hide = "hide";
+}
