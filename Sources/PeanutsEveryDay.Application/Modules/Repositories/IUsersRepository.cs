594a29107118987c9f23d950ca0c1f7ca41bfcc3
Reswero <reswero@mail.ru>
2023-08-06 13:01:16 +0600

    Added Time Comics Sender Service

--- Sources/PeanutsEveryDay.Application/Modules/Repositories/IUsersRepository.cs
+++ Sources/PeanutsEveryDay.Application/Modules/Repositories/IUsersRepository.cs
@@ -1,10 +1,15 @@
 ﻿using PeanutsEveryDay.Domain.Models;
+using System.Linq.Expressions;
 
 namespace PeanutsEveryDay.Application.Modules.Repositories;
 
 public interface IUsersRepository
 {
-    public Task AddAsync(User user, CancellationToken cancellation = default);
-    public Task<User?> GetAsync(long id, CancellationToken cancellation = default);
-    public Task UpdateAsync(User user, CancellationToken cancellation = default);
+    public Task AddAsync(User user, CancellationToken cancellationToken = default);
+    public Task<User?> GetAsync(long id, CancellationToken cancellationToken = default);
+    public Task UpdateAsync(User user, CancellationToken cancellationToken = default);
+
+    public Task<List<User>> GetAllAsync(CancellationToken cancellationToken = default);
+    public Task<List<User>> GetByFilterAsync(Expression<Func<User, bool>> filter,
+        CancellationToken cancellationToken = default);
 }
